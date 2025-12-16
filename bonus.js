1553cb1396fc5abce23dbc7159e0b56ace31d862
Omar-MK-20 <omar.mk.201@gmail.com>
2025-12-16 13:07:40 +0200

    Solving majorityElement LeetCode question

--- bonus.js
+++ bonus.js
@@ -4,8 +4,26 @@
  */
 var majorityElement = function (nums)
 {
-    for (let i = 0; i < nums.length; i++) {
-        
-        
+    let majEle = [nums[0], 0];
+    for (let i = 0; i < nums.length; i++)
+    {
+        if (majEle[1] == 0)
+        {
+            majEle[0] = nums[i];
+            majEle[1]++;
+        }
+        else
+        {
+            majEle[1] += (majEle[0] == nums[i]) ? 1 : -1;
+        }
     }
+    return majEle[0];
 };
+
+
+
+
+const nums = [2, 1, 1, 1];
+console.log(majorityElement(nums))
+
+
